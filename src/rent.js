2f296605f69a5b31f68dd7741ba363d57d51f322
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-08-01 11:49:03 +0300

    changing-the-input-during-autofill-and-resetting-the-values-from-the-form

--- src/rent.js
+++ src/rent.js
@@ -2,24 +2,35 @@
     const refs = {
         form: document.getElementById("yacht-form"),
         modal: document.querySelector("[data-modal-rent]"),
-        
         closeModalBtn: document.querySelector("[data-modal-close-rent]"),
+        inputs: document.querySelectorAll("input, textarea"),
     };
-
+  
     refs.form.addEventListener("submit", handleSubmit);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-
+    refs.closeModalBtn.addEventListener("click", () => {
+        toggleModal();
+        resetForm();
+    });
+  
     function handleSubmit(event) {
         event.preventDefault();
-
+  
         const inputs = refs.form.querySelectorAll("input[required], textarea[required]");
         const isFormValid = Array.from(inputs).every(input => input.value.trim() !== '');
         if (isFormValid) {
             toggleModal();
         }
     }
-
+  
     function toggleModal() {
         refs.modal.classList.toggle("is-hidden");
     }
-})();
+  
+    function resetForm() {
+        refs.inputs.forEach(input => {
+            input.value = '';
+        });
+    }
+  })();
+
+  
